3bdef5d153ec22286962ac8907c6371bd59a9465
rubykat <rubykat>
2003-02-02 10:47:49 +0000

    KA: Added the PlainBow theme

---
+++ themes/plainbow/background.cpp
@@ -0,0 +1,9 @@
+DestroyFunc FuncFvwmStartThemeBackground
+AddToFunc   FuncFvwmStartThemeBackground
++ I Exec xsetroot -mod 2 4 -fg DEF_DARK -bg DEF_MID
+
+DestroyFunc FuncFvwmStopThemeBackground
+AddToFunc   FuncFvwmStopThemeBackground
++ I Nop
+
+
