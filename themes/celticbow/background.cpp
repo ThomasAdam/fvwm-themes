0f356e73608101315686e6b25115e999aaf704ed
thomasadam <thomasadam>
2008-03-25 01:11:23 +0000

    Update celticbow theme to use features in FVWM 2.5.25+
    Some of the style options were a little out of date.  This also removes some
    deprecation warnings from old style changing since the theme was written.

--- themes/celticbow/background.cpp
+++ themes/celticbow/background.cpp
@@ -1,12 +1,12 @@
 DestroyFunc FuncFvwmStartThemeBackground
 AddToFunc   FuncFvwmStartThemeBackground
 #if (DEF_STRENGTH == 1)
-+ I Exec convert -modulate 125,80,CS_HUE_PERCENT $./images/background/red_lace10.png - | display -window root -
++ I Exec exec convert -modulate 125,80,CS_HUE_PERCENT $./images/background/red_lace10.png - | display -window root -
 #else
 #if (DEF_STRENGTH == 3)
-+ I Exec convert -modulate 75,140,CS_HUE_PERCENT $./images/background/red_lace10.png - | display -window root -
++ I Exec exec convert -modulate 75,140,CS_HUE_PERCENT $./images/background/red_lace10.png - | display -window root -
 #else
-+ I Exec convert -modulate 100,100,CS_HUE_PERCENT $./images/background/red_lace10.png - | display -window root -
++ I Exec exec convert -modulate 100,100,CS_HUE_PERCENT $./images/background/red_lace10.png - | display -window root -
 #endif
 #endif
 
